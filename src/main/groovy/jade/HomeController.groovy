3ba68f8c7227014ce73bf663d0d9566b5db70d81
josdem <joseluis.delacruz@gmail.com>
2015-08-03 18:27:24 -0500

    Adding a model to the page

--- src/main/groovy/jade/HomeController.groovy
+++ src/main/groovy/jade/HomeController.groovy
@@ -3,12 +3,14 @@ package jade
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.ui.Model
 
 @Controller
 class HomeController {
 
   @RequestMapping('/')
-  String index() {
+  String index(Model model) {
+    model.addAttribute 'currentTime', new Date()
     'index'
   }
 
